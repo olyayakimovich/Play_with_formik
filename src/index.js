b170943544b283643f35401ea10fca5588308fe8
Olya Yakimovich <olgayakimovich87@gmail.com>
2020-01-01 16:45:21 +0300

    apply validation using context

--- src/index.js
+++ src/index.js
@@ -1,65 +1,83 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { useFormik } from 'formik';
-
-const validate = values => {
-  const errors = {};
-  if (!values.firstName) {
-    errors.firstName = 'Required';
-  } else if (values.firstName.length > 15) {
-    errors.firstName = 'Must be 15 characters or less';
-  }
-
-  if (!values.lastName) {
-    errors.lastName = 'Required';
-  } else if (values.lastName.length > 20) {
-    errors.lastName = 'Must be 20 characters or less';
-  }
-
-  if (!values.email) {
-    errors.email = 'Required';
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-    errors.email = 'Invalid email address';
-  }
-
-  return errors;
-};
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 const SignupForm = () => {
-  // Pass the useFormik() hook initial form values and a submit function that will
-  // be called when the form is submitted
-  const { handleSubmit, errors, touched, getFieldProps } = useFormik({
-    initialValues: {
-      email: '',
-      firstName: '',
-      lastName: ''
-    },
-    validate,
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
-    }
-  });
-
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="firstName">First Name</label>
-      <input name="firstName" {...getFieldProps('firstName')} />
-      {touched.firstName && errors.firstName ? (
-        <div>{errors.firstName}</div>
-      ) : null}
-      <label htmlFor="lastName">Last Name</label>
-      <input name="lastName" {...getFieldProps('lastName')} />
-      {touched.lastName && errors.lastName ? (
-        <div>{errors.lastName}</div>
-      ) : null}
-      <label htmlFor="email">Email Address</label>
-      <input name="email" {...getFieldProps('email')} />
-      {touched.email && errors.email ? <div>{errors.email}</div> : null}
-      <button type="submit">Submit</button>
-    </form>
+    <Formik
+      initialValues={{ firstName: '', lastName: '', email: '' }}
+      validationSchema={Yup.object({
+        firstName: Yup.string()
+          .max(15, 'Must be 15 characters or less')
+          .required('Required'),
+        lastName: Yup.string()
+          .max(20, 'Must be 20 characters or less')
+          .required('Required'),
+        email: Yup.string()
+          .email('Invalid email address')
+          .required('Required')
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      <Form>
+        <label htmlFor="firstName">First Name</label>
+        <Field name="firstName" type="text" />
+        <ErrorMessage name="firstName" />
+        <label htmlFor="lastName">Last Name</label>
+        <Field name="lastName" type="text" />
+        <ErrorMessage name="lastName" />
+        <label htmlFor="email">Email Address</label>
+        <Field name="email" type="email" />
+        <ErrorMessage name="email" />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
   );
 };
 
 // ========================================
 
 ReactDOM.render(<SignupForm />, document.getElementById('root'));
+
+// <input className="form-input" placeHolder="Jane"  />
+{
+  /* <Field name="firstName" className="form-input" placeholder="Jane" /> */
+}
+
+// <textarea className="form-textarea"/></textarea>
+{
+  /* <Field name="message" as="textarea"  className="form-input"/> */
+}
+
+// <select className="my-select"/>
+{
+  /* <Field name="colors" as="select" className="my-select">
+  <option value="red">Red</option>
+  <option value="green">Green</option>
+  <option value="blue">Blue</option>
+</Field> */
+}
+
+// with styled-components/emotion
+// const MyStyledInput = styled.input`
+//   padding: .5em;
+//   border: 1px solid #eee;
+//   /* ... */
+// `
+// const MyStyledTextarea = MyStyledInput.withComponent('textarea');
+
+// <input className="czx_123" placeHolder="google.com"  />
+{
+  /* <Field name="website" as={MyStyledInput} placeHolder="google.com"/> */
+}
+
+// <textarea  placeHolder="Post a message..." rows={5}></textarea>
+{
+  /* <Field name="message" as={MyStyledTextArea} placeHolder="Post a message.." rows={4}/> */
+}
