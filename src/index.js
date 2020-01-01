94c56f326e4c18db4f0d868cf2c0b202136b33b3
Olya Yakimovich <olgayakimovich87@gmail.com>
2020-01-01 16:17:13 +0300

    formik validation with useFormik hook

--- src/index.js
+++ src/index.js
@@ -1,12 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { useFormik } from 'formik';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const validate = values => {
+  const errors = {};
+  if (!values.firstName) {
+    errors.firstName = 'Required';
+  } else if (values.firstName.length > 15) {
+    errors.firstName = 'Must be 15 characters or less';
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  if (!values.lastName) {
+    errors.lastName = 'Required';
+  } else if (values.lastName.length > 20) {
+    errors.lastName = 'Must be 20 characters or less';
+  }
+
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+
+  return errors;
+};
+
+const SignupForm = () => {
+  // Pass the useFormik() hook initial form values and a submit function that will
+  // be called when the form is submitted
+  const { handleSubmit, errors, touched, getFieldProps } = useFormik({
+    initialValues: {
+      email: '',
+      firstName: '',
+      lastName: ''
+    },
+    validate,
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    }
+  });
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="firstName">First Name</label>
+      <input name="firstName" {...getFieldProps('firstName')} />
+      {touched.firstName && errors.firstName ? (
+        <div>{errors.firstName}</div>
+      ) : null}
+      <label htmlFor="lastName">Last Name</label>
+      <input name="lastName" {...getFieldProps('lastName')} />
+      {touched.lastName && errors.lastName ? (
+        <div>{errors.lastName}</div>
+      ) : null}
+      <label htmlFor="email">Email Address</label>
+      <input name="email" {...getFieldProps('email')} />
+      {touched.email && errors.email ? <div>{errors.email}</div> : null}
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+// ========================================
+
+ReactDOM.render(<SignupForm />, document.getElementById('root'));
